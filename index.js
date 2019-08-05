91e0712b09e1a797c421cd919361ba9cb26c8921
Sean Hetzel <seanhetzel1@gmail.com>
2019-08-05 18:13:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() * 100);
+const mod = 9 % 5;
+const max = Math.max(1,2,3,20);
