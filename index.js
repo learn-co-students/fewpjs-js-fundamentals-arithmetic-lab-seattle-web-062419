16cc0c02bea2200003060ec569c4902dd8cfa668
github username <github email address>
2019-08-05 14:06:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+let max = Math.max(5,10,15,20);
+
+let random = (Math.random() * 10) + 1;
+
+let mod = 29 % 5;
